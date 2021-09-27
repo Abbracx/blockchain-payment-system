2409509b011f00201f8d022da220abf52cc81b25
Abbracx <tankoraphael@gmail.com>
2021-09-27 08:17:53 +0100

    improved makePayment function

--- contracts/PaymentProcessor.sol
+++ contracts/PaymentProcessor.sol
@@ -11,7 +11,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 contract PaymentProcessor is Ownable {
 
     IERC20 private UJToken;
-    uint private SCHOOL_FEES_PRICE;
+    uint private SCHOOL_FEES_PRICE = 1 ether;
 
     event LogPayment(address indexed who,
                       address indexed to,
@@ -37,15 +37,18 @@ contract PaymentProcessor is Ownable {
  * function so that owner/deployer of contract can make withdrawal at particular token threshold..
  */
 
-  modifier checkAmount() {
-    if(msg.value < SCHOOL_FEES_PRICE) revert();
+  modifier checkAmount(uint _payer_amount) {
+    if(_payer_amount < SCHOOL_FEES_PRICE) revert('Insufficient funds.');
     _;
   }
 
   function makePayment(address indexed _payer_address, string memory _payer, uint _payer_amount) external payable{
     require (_payer_address != address(0), 'PaymentProcessor: Invalid Address.');
-    UJToken.transferFrom(_payer_address, payable(address(this)), _payer_amount);
-    emit LogPayment(_payer_address, address(this), _payer, _amount, block.timestamp);
+    if(_payer_amount >= SCHOOL_FEES_PRICE){
+      UJToken.transferFrom(_payer_address, payable(address(this)), _payer_amount);
+      emit LogPayment(_payer_address, address(this), _payer, _amount, block.timestamp);
+    }
+    revert('Insufficient payment fee.');
   }
 
   function setSchoolfFees(uint _amount) external {
@@ -58,15 +61,15 @@ contract PaymentProcessor is Ownable {
 
   function withdrawal() external returns(bool){
     require(owner() == _msgSender(), 'Ownable: Only owner can initiate transaction.');
-
+    require(address(this).balance > 0, "Cant withdraw 0 UJT")
     uint balanceBeforeTransfer = address(this).balance;
-    (bool success, ) = payable(_msgSender()).call{value:balanceBeforeTransfer}("");
+    (bool success, ) = payable(_msgSender()).call{value:balanceBeforeTransfer}(""); //Read About these again
 
     if(success){
       emit LogWidrawal(_msgSender(), balanceBeforeTransfer);
-      return true
+      return true;
     }
-    revert('Transaction Failed.');
+    return false;
   }
 
   function refund(address payable reciever, uint amount) external{
